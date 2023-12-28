48ef77d449c6e3cf86e6f739069d8bd746a597e2
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2023-12-28 21:28:02 +0530

    Apierror handler , common component

--- company-server/src/utils/ApiError.js
+++ company-server/src/utils/ApiError.js
@@ -1,17 +1,7 @@
-export const ApiError = (
-    statusCode = 500,
-    message = "Internal Server Error."
-) => {
-    const error = new Error();
-    error.status = statusCode;
-    error.message = message;
-    return error;
-};
-
-// try {
-//     throw ApiError();
-// } catch (error) {
-//     console.log(error);
-//     console.log(error.name);
-//     console.log(error.message);
-// }
+export class ApiError extends Error {
+    constructor(statusCode = 500, message = "Internal Server Error.") {
+        super(message);
+        this.status = statusCode;
+        this.name = "ApiError";
+    }
+}
