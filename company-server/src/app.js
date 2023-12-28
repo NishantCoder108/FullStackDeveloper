6c8332664fd52e9fd6cebb921762a1508deda145
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2023-12-28 23:56:50 +0530

    Logout user , set refresh token null

--- company-server/src/app.js
+++ company-server/src/app.js
@@ -10,8 +10,8 @@ app.use(
         origin: process.env.ACCESS_ORIGIN,
     })
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
@@ -22,4 +22,5 @@ import userRouter from "./routes/user.routes.js";
 
 app.use("/api/v1/users", userRouter);
 
+// http://localhost:8000/api/v1/users/register
 export { app };
--- company-server/src/controllers/employee.controllers.js
+++ company-server/src/controllers/employee.controllers.js
@@ -151,3 +151,28 @@ export const Login = async (req, res) => {
       - "/logout" , first jwt verify of token, 
       - 
  */
+
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined,
+            },
+        },
+        {
+            new: true,
+        }
+    );
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    };
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User logged Out"));
+});
