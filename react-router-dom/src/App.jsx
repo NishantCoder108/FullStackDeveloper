d37985cdf1d2adb3c935817324b53032a7415fdf
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-01-04 02:59:25 +0530

    Routes setup with jsx method, createRoutesFromElements ,small improve in code

--- react-router-dom/src/App.jsx
+++ react-router-dom/src/App.jsx
@@ -4,31 +4,59 @@ import {
     Route,
     createRoutesFromElements,
     Link,
+    Outlet,
 } from "react-router-dom";
 import "./App.css";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: (
-            <>
-                Home Page <Link to="/about">About </Link>
-            </>
-        ),
-        errorElement: "Error page",
-    },
-    {
-        path: "about",
-        element: (
-            <>
-                About page <Link to="/">HoME</Link>
-            </>
-        ),
-    },
-]);
+// const router = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: (
+//             <>
+//                 Home Page <Link to="/about">About </Link>
+//             </>
+//         ),
+//         errorElement: "Error page",
+//     },
+//     {
+//         path: "about",
+//         element: (
+//             <>
+//                 About page <Link to="/">HoME</Link>
+//             </>
+//         ),
+//     },
+// ]);
 
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route
+            path="/"
+            element={
+                <div>
+                    <h2>Root</h2> <Outlet />{" "}
+                </div>
+            }
+            errorElement={<p>404 Not Found</p>}
+        >
+            <Route path="about" element={<h1>This is About page</h1>} />
+            <Route path="contact" element={<h2>This is contact page</h2>} />
+        </Route>
+    )
+);
 const App = () => {
     return <RouterProvider router={router} />;
 };
 
 export default App;
+
+/**
+ * ----Routing setup , we can these two method------
+ *
+ *  1. JSX method    -->Recommended for ,easy to understand
+ *  2. Object method  --> it sometime hard in nested routes
+ *
+ *
+ *
+ * so, here , we use Jsx method
+ */
