f659691eaab750eb5278dedd35cdd5cf59efa34f
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-02-04 16:56:27 +0530

    Share on twitter functionalies added

--- quotes-generate/src/App.jsx
+++ quotes-generate/src/App.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 
 import "./App.css";
-import { quotesUrl } from "./constants";
-import { Box, Container, CssBaseline, Typography } from "@mui/material";
+import { Box, Button, Container, CssBaseline, Typography } from "@mui/material";
 import QuotesCard from "./components/QuotesCard";
 
 function App() {
@@ -10,9 +9,22 @@ function App() {
     const defaultQuote =
         "All our dreams can come true, if we have the courage to pursue them.";
 
-    const randomQuote =
-        quotes?.[Math.floor(Math.random() * 18)]?.text || defaultQuote;
+    const [quote, setQuote] = useState(defaultQuote);
 
+    const generateQuote = () => {
+        const randomQuote =
+            quotes?.[Math.floor(Math.random() * quotes.length)]?.text ||
+            defaultQuote;
+        setQuote(randomQuote);
+    };
+
+    const tweetQuote = () => {
+        const tweetTemp = "Quotes of the Day 💪: " + '"' + quote + '"';
+        const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+            tweetTemp
+        )}`;
+        window.open(tweetUrl);
+    };
     const fetchQuotes = async () => {
         try {
             const fetchUrlData = await fetch("https://type.fit/api/quotes");
@@ -51,7 +63,32 @@ function App() {
                         Quotes Generator
                     </Typography>
 
-                    <QuotesCard quote={randomQuote} />
+                    <QuotesCard quote={quote} />
+
+                    <Box
+                        sx={{
+                            display: "flex",
+                            justifyContent: "space-evenly",
+                            alignItems: "center",
+                            gap: "1rem",
+                            padding: "2rem",
+                        }}
+                    >
+                        <Button
+                            variant="outlined"
+                            color="primary"
+                            onClick={generateQuote}
+                        >
+                            New Quote{" "}
+                        </Button>
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                            onClick={tweetQuote}
+                        >
+                            Share on Twitter{" "}
+                        </Button>
+                    </Box>
                 </Box>
             </Container>
         </>
